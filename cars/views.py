36c7aeb73c15b2b9546135ae7d04354227836858
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-01-12 19:07:09 -0300

    Indentação de Views

--- cars/views.py
+++ cars/views.py
@@ -3,15 +3,8 @@ from django.db.models import Q
 from cars.models import Cars
 from cars.forms import CarModelForm
 from django.views import View
-def cars_view(request):
-    search = request.GET.get('search')
-    cars = Cars.objects.all().order_by('model')
+from django.views.generic import ListView
 
-    if search:
-         cars = cars.filter(Q(model__icontains=search) | Q(brand__name__icontains=search))#Uso de Q objects: Isso permite combinar múltiplas condições de filtragem em uma única consulta, melhorando a legibilidade e a eficiência.
-    return render(request,
-                  'cars.html',
-                   {'cars': cars})
 
 class CarsView(View):
 
@@ -21,16 +14,21 @@ class CarsView(View):
 
         if search:
             cars = cars.filter(Q(model__icontains=search) | Q(brand__name__icontains=search))#Uso de Q objects: Isso permite combinar múltiplas condições de filtragem em uma única consulta, melhorando a legibilidade e a eficiência.
-        return render(request,
+        return render(
+            request,
                     'cars.html',
                     {'cars': cars})
 
-def new_car_view(request):
-    new_car_form = CarModelForm()
-    if request.method == 'POST':
+
+
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-
-    return render(request, 'new_car.html',{'new_car_form': new_car_form})
+        return render (request, 'new_car.html', {'new_car_form': new_car_form})
