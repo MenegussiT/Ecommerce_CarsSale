1640338c1c813557aeea32c292e6e23649876974
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-01-12 19:37:47 -0300

    Alteração no New Car View

--- cars/views.py
+++ cars/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from cars.models import Cars
 from cars.forms import CarModelForm
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 
 class CarsView(ListView):
@@ -18,14 +18,8 @@ class CarsView(ListView):
         return cars_query
 
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
-    
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render (request, 'new_car.html', {'new_car_form': new_car_form})
+class NewCarView(CreateView):
+    model = Cars
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars'
