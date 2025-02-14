ef0d586ef372760c38515f8b7b2ac24ca1c04dd3
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-02-13 22:04:35 -0300

    Criando a lógica de Update e Delete do inventário.

--- cars/signals.py
+++ cars/signals.py
@@ -1,19 +1,33 @@
 from django.db.models.signals import post_save, pre_save, pre_delete, post_delete
+from django.db.models import Sum
 from django.dispatch import receiver
-from .models import Cars
+from .models import Cars, CarInventory
+
+def car_inventory_update():
+    cars_count = Cars.objects.count()
+    cars_value = Cars.objects.aggregate(total_value=Sum('price'))['total_value']
+    CarInventory.objects.create(
+        cars_count=cars_count, 
+        cars_value=cars_value
+    )
+
 
 @receiver(post_save, sender=Cars)
 def car_post_save(sender, instance, created, **kwargs):
-    print('### POST SAVE ####')
+   if created:
+    car_inventory_update()  
+
 
 @receiver(pre_save, sender=Cars)
 def car_pre_save(sender, instance, **kwargs):
     print('### PRE SAVE ####')
 
+
 @receiver(pre_delete, sender=Cars)
 def car_pre_delete(sender, instance, **kwargs):
     print('### PRE DELETE ####')
 
+
 @receiver(post_delete, sender=Cars)
 def car_post_delete(sender, instance, **kwargs):
-    print('### POST DELETE ####')
+    car_inventory_update()
