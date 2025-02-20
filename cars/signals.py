c6f366e8ce5bddb7b0b3c0eafe6c6b79b8798966
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-02-19 23:18:35 -0300

    Integrando o método de descrição automática com a API do CHATGPT. VERIFICAR AINDA

--- cars/signals.py
+++ cars/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, pre_save, pre_delete, post_delet
 from django.db.models import Sum
 from django.dispatch import receiver
 from .models import Cars, CarInventory
+from openai_api.client import get_car_ai_bio
 
 def car_inventory_update():
     cars_count = Cars.objects.count()
@@ -20,7 +21,13 @@ def car_post_save(sender, instance, created, **kwargs):
 
 @receiver(pre_save, sender=Cars)
 def car_pre_save(sender, instance, **kwargs):
-    print('### PRE SAVE ####')
+
+    if not instance.bio:
+       ai_bio = get_car_ai_bio(
+          instance.model, 
+          instance.brand.name, 
+          instance.year)
+       instance.bio = ai_bio
 
 
 @receiver(pre_delete, sender=Cars)
@@ -31,3 +38,4 @@ def car_pre_delete(sender, instance, **kwargs):
 @receiver(post_delete, sender=Cars)
 def car_post_delete(sender, instance, **kwargs):
     car_inventory_update()
+
