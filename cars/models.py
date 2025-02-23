f713b27520365f951640cc94361237db33f72dbe
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-02-23 19:15:50 -0300

    Adicionando uma imagem padrão

--- cars/models.py
+++ cars/models.py
@@ -18,7 +18,7 @@ class Cars(models.Model):
     price = models.FloatField(blank=True, null=True)
     color = models.CharField(max_length=50)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
-    picture = models.ImageField(upload_to='cars/cars_photo', blank=True, null=True)
+    picture = models.ImageField(upload_to='cars/cars_photo', blank=True, null=True, default='cars/cars_photo/default.jpg')
     bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
