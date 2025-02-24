f72ade55fc0e654b484cef45b071dd447f9b7777
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-02-23 21:38:20 -0300

    Alteração do prompt da API

--- gemini_api/client.py
+++ gemini_api/client.py
@@ -6,7 +6,7 @@ def get_car_ai_bio(model, brand, year):
     model_gen = genai.GenerativeModel("gemini-2.0-flash")
 
     
-    prompt = f"Você agora é um vendedor de automóveis experiente. Me mostre uma descrição do carro modelo {model} da marca {brand} do ano {year} em até 502 caracteres."
+    prompt = f" Me mostre uma descrição do carro modelo {model} da marca {brand} do ano {year} em até 502 caracteres de forma técnica e objetiva, sem o uso de ásteristico para negrito ou coisas parecidas."
 
     response = model_gen.generate_content(prompt)
 
