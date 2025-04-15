d1e13e8f8c9e9aa6c4c06295c1e550b38f41d92c
MenegussiT <felipe.menegussi.faria@gmail.com>
2025-04-15 20:13:08 -0300

    Alterando site portifólio

--- app/settings.py
+++ app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cars',
         'USER' : 'postgres',
-        'PASSWORD' : 'Swift03@',
+        'PASSWORD' : '...',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
